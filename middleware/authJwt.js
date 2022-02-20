af683c2910cd0c8c44c839868b9ac87e97e76975
miq28 <iqbal.tiro@gmail.com>
2022-02-20 08:13:46 +0700

    test1

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -2,25 +2,78 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = function (req, res, next) {
     try {
-        let accessToken
-        if (req.headers['authorization']) {
-            const authHeader = req.headers['authorization']
-            accessToken = authHeader && authHeader.split(' ')[1]
-        } else {
-            accessToken = req.cookies.jwtAccess
+        var size = 0
+
+        var gotData = function (d) {
+            size += d.length; // add this chunk's size to the total number of bytes received thus far
+            console.log('upload chunk', size);
+
+            let accessToken
+            if (req.headers['authorization']) {
+                const authHeader = req.headers['authorization']
+                accessToken = authHeader && authHeader.split(' ')[1]
+            } else {
+                accessToken = req.cookies.jwtAccess
+            }
+
+            //use the jwt.verify method to verify the access token
+            //throws an error if the token has expired or has a invalid signature
+            // let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+            let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, function (err) {
+                if (err) {
+                    console.log('aborting request');
+                    req.removeListener('data', gotData);
+                    req.removeListener('end', reqEnd);
+                    // res.header('Connection', 'close');
+                    // return res.send(413, 'Upload too large');
+                    // throw new Error(err.message)
+                    next(err)
+                } else {
+                    decoded.expireIn = decoded.exp - Math.floor(Date.now() / 1000)
+                    // console.log('jwt_access_token', decoded)
+
+                    // attach jwt info to req.user
+                    req.user = decoded
+                }
+            })
+
+        };
+
+        var reqEnd = function () {
+            res.send('ok, got ' + size + ' bytes');
         }
 
-        //use the jwt.verify method to verify the access token
-        //throws an error if the token has expired or has a invalid signature
-        // let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-        let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-        decoded.expireIn = decoded.exp - Math.floor(Date.now() / 1000)
-        // console.log('jwt_access_token', decoded)
+        req.on('data', gotData);
 
-        // attach jwt info to req.user
-        req.user = decoded
+        req.on('end', reqEnd);
 
-        next()
+
+        if (req.is('application/*')) {
+            var size = 0;
+            let decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+            decoded.expireIn = decoded.exp - Math.floor(Date.now() / 1000)
+            // console.log('jwt_access_token', decoded)
+
+            // attach jwt info to req.user
+            req.user = decoded
+
+            next()
+        }
+
+
+
+
+
+
+        // var reqEnd = function() {
+        //    res.send('ok, got ' + size + ' bytes');
+        // }
+
+        // req.on('data', gotData);
+
+        // req.on('end', reqEnd);
+
+        
     }
     catch (err) {
         //if an error occured return request unauthorized error
